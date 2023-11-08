0ca2766126c6667357e559e538b4e0183018c0bc
Chen001117 <cwz19@mails.tsinghua.edu.cn>
2023-11-08 17:34:11 -0500

    support smacv2 protoss_5v5

--- onpolicy/runner/shared/base_runner.py
+++ onpolicy/runner/shared/base_runner.py
@@ -81,7 +81,6 @@ class Runner(object):
                             self.envs.observation_space[0],
                             share_observation_space,
                             self.envs.action_space[0],
-                            self.num_agents, # default 2
                             device = self.device)
 
         if self.model_dir is not None:
--- onpolicy/scripts/train/train_smac.py
+++ onpolicy/scripts/train/train_smac.py
@@ -174,8 +174,8 @@ def main(args):
 
     if all_args.use_wandb:
         run = wandb.init(config=all_args,
-                         project=all_args.env_name,
-                         entity=all_args.user_name,
+                         project="SMAC",
+                         entity="cwz19",
                          notes=socket.gethostname(),
                          name=str(all_args.algorithm_name) + "_" +
                               str(all_args.experiment_name) + "_" + 
