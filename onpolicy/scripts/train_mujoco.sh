7b7f5cc8220c86c50bf53cbad19f450970e072dc
Chen001117 <cwz19@mails.tsinghua.edu.cn>
2024-02-11 15:11:32 -0800

    exp

--- onpolicy/scripts/train_mujoco.sh
+++ onpolicy/scripts/train_mujoco.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 env="MuJoCo"
 scenario="Walker2d-v3"
-num_agents=2
+num_agents=3
 algo="rmappo"
 exp="check"
 
@@ -13,7 +13,7 @@ git commit -m 'exp'
 CUDA_VISIBLE_DEVICES=0 python3 train/train_mujoco.py --env_name ${env} \
     --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
     --num_agents ${num_agents} --seed ${seed} --n_training_threads 1 --n_rollout_threads 16 \
-    --num_mini_batch 4 --episode_length 512 --use_ReLU --num_env_steps 1000000000 \
+    --num_mini_batch 4 --episode_length 1024 --use_ReLU --num_env_steps 1000000000 \
     --ppo_epoch 5 --entropy_coef 0 --hidden_size 64 --log_interval 1 \
     --critic_hidden_size 128 --data_chunk_length 8 --lr 3e-5 --critic_lr 3e-5 \
     --wandb_name "cwz19" --user_name "cwz19" \
