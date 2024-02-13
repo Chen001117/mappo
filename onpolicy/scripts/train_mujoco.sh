547ac3064c3dbf311248caeaf0002ed7a13a467e
Chen001117 <cwz19@mails.tsinghua.edu.cn>
2024-02-12 22:04:46 -0800

    exp

--- onpolicy/scripts/train_mujoco.sh
+++ onpolicy/scripts/train_mujoco.sh
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=0 python3 train/train_mujoco.py --env_name ${env} \
     --num_agents ${num_agents} --seed ${seed} --n_training_threads 1 --n_rollout_threads 16 \
     --num_mini_batch 4 --episode_length 1024 --use_ReLU --num_env_steps 1000000000 \
     --ppo_epoch 5 --entropy_coef 0 --hidden_size 64 --log_interval 1 \
-    --critic_hidden_size 128 --data_chunk_length 8 --lr 0 --critic_lr 5e-5 \
+    --critic_hidden_size 128 --data_chunk_length 8 --lr 5e-5 --critic_lr 5e-5 \
     --wandb_name "cwz19" --user_name "cwz19" \
-    --model_dir "results/MuJoCo/Walker2d-v3/rmappo/check/wandb/run-20240110_224445-yhuytopt/files" 
+    --model_dir "results/MuJoCo/Walker2d-v3/rmappo/check/wandb/run-20240212_090809-51f1jrze/files" 
 done
